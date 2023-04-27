c3af535d4a0be2c830f1f7912467754c604b5450
Nick Armfield <bizzyn3@gmail.com>
2023-04-27 10:45:43 +0300

    feat: add pressing keys on a physical keyboard functional

---
+++ app.js
@@ -0,0 +1,30 @@
+document.onkeydown = (event) => {
+  let allKey = document.querySelectorAll(`[data-key="${event.code}"]`)
+  for (let key of allKey) {
+    key?.classList.add("pressed")
+  }
+  if (event.shiftKey) {
+    let shiftKey = document.querySelectorAll('.shift')
+    for (let shift of shiftKey) {
+      shift.classList.add('pressed')
+    }
+    document.querySelector('.keyboard').classList.add("shifted")
+  }
+  if (event.shiftKey && event.ctrlKey) {
+    document.querySelector('.keyboard').classList.toggle("russian")
+  }
+}
+
+document.onkeyup = (event) => {
+  let allKey = document.querySelectorAll(`[data-key="${event.code}"]`)
+  for (let key of allKey) {
+    key?.classList.remove("pressed")
+  }
+  if (event.key = ('Shift')) {
+    let shiftKey = document.querySelectorAll('.shift')
+    for (let shift of shiftKey) {
+      shift.classList.remove('pressed')
+    }
+    document.querySelector('.keyboard').classList.remove("shifted")
+  }
+}
